aa62bd9536325b7c2dc4934c6bac6fcb44cefc5b
valesem <54368000+valesem@users.noreply.github.com>
2019-08-21 11:39:07 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Erin Vales");
+		System.out.println("Hello");
+		System.out.println("Write a code for Hello");
+		System.out.println("Aug 21 2019");
+		System.out.println("CMSC255-004");
+		System.out.println();
+	}
+
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
